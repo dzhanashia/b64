6c8029c7d9d761b395de49f3af6af957d2c8c899
dzhanashia <46886560+dzhanashia@users.noreply.github.com>
2019-01-27 22:16:45 +0300

    Add files via upload

--- README.txt
+++ README.txt
@@ -1,16 +1,26 @@
+Установка через pip:
+
+pip install matplotlib 
+
+pip install scikit-learn
+
+pip install -i https://test.pypi.org/simple/ b64detector-dzh==0.2
+
 Функции:
 
-isb64(s, log=True)
-Проверяет является ли s (string/file) b64.
+isb64(s)
+Проверяет является ли s (string) b64.
 Возвращает True если является, False иначе.
-log определяет включено ли логирование во время выполнения функции (по умолчанию True)
 
-pick_out(s, index=True)
-Делит s (string/file) на b64 и нет.
-Возвращает индексы b64 слов если index=True, иначе 2 списка - список с b64 и список с остальными словами (по умолчанию True)
+pick_out(s)
+Делит s (string) на b64 и не b64.
+Возвращает 2 списка - список с b64 и список с остальными словами
+
+pick_out42(s):
+Делит s (string) на фрагменты длинной 42 символа и проверяет их. 
+Возвращает лист с индексами b64 фрагментов.
 
 delete_clfs()
 Удаляет файлы с обученными классификаторами
 
----
-SVC, KNeighborsClassifier, GaussianNB... - обученные классификаторы
+Обученные классификаторы располагаются в ./trained_clf
--- isb64.py
+++ isb64.py
@@ -1,5 +1,6 @@
 import logging
 import os.path
+import configparser
 
 import matplotlib.pyplot as plt
 import numpy as np
@@ -14,105 +15,129 @@ from sklearn.neural_network import MLPClassifier
 from sklearn.svm import SVC
 from sklearn.tree import DecisionTreeClassifier
 
+logging.getLogger(__name__)
+logging.basicConfig(level=logging.INFO)
+
+dirname = os.path.dirname(__file__)
+
+config = configparser.ConfigParser()
+config._interpolation = configparser.ExtendedInterpolation()
+config.read(os.path.join(dirname, "config.ini"))
+
+if not os.path.exists(os.path.join(dirname, 'trained_clfs')):
+    os.makedirs(os.path.join(dirname, 'trained_clfs'))
+
 classifiers = [
-        SVC(kernel='linear', C=0.025, probability=True),
         neighbors.KNeighborsClassifier(2, weights='uniform'),
         DecisionTreeClassifier(max_depth=5),
         RandomForestClassifier(max_depth=5, n_estimators=5, max_features=1),
         MLPClassifier(alpha=1),
         AdaBoostClassifier(),
         GaussianNB(),
-        QuadraticDiscriminantAnalysis()
+        QuadraticDiscriminantAnalysis(),
+        SVC(kernel='linear', C=0.025, probability=True)
         ]
 
-logging.getLogger(__name__)
-logging.basicConfig(level=logging.INFO)
+
+def _check_clf(clf):
+    """ Trains classifier if there isn't a trained one"""
+    if not os.path.exists(os.path.join(dirname, ('trained_clfs/' + clf.__class__.__name__))):
+        logging.info('Trained ' + clf.__class__.__name__ + ' not found. Training...')
+        _clf_train(clf)
 
 
-def _name(clf):
-    return str(clf).partition('(')[0]
-
-
-def _initialize(clf):
-    if not os.path.isfile(_name(clf)):
-        logging.info('Trained ' + _name(clf) + ' not found. Training...')
-        main(test=False, clfs=(clf,))
-
-
-def symbols_allowed(s):
-    s = s[:32]
-    for symb in s:
-        if not ('A' <= symb <= 'Z' or 'a' <= symb <= 'z' or '0' <= symb <= '9' or symb == '+' or symb == '=' or symb == '\h'[:-1]):
+def check_symbols(word):
+    """ Checks whether word doesn't have prohibited symbols """
+    for symbol in word:
+        if not ('A' <= symbol <= 'Z' or 'a' <= symbol <= 'z' or '0' <= symbol <= '9'
+                or symbol == '+' or symbol == '=' or symbol == '\\'):
             return False
     return True
 
 
-def _get_features(s):
-    s = s[:32]
-    return [len(s), sum(c.isdigit() for c in s) / len(s), sum(c.islower() for c in s) / len(s), s.count('=')]
+def _get_features(word):
+    """ Returns list of features"""
+    return [
+        len(word),
+        sum(symbol.isdigit() for symbol in word) / len(word),
+        sum(symbol.islower() for symbol in word) / len(word),
+        word.count('=')
+    ]
 
 
-def isb64(s, log=True, clfs=classifiers):
+def isb64(s, clfs=classifiers):
+    """Check if s is b64 encoded
+
+    Arguments:
+    s -- string that needs to be checked
+
+    Returns:
+    True if s is b64 encoded, False otherwise.
+    """
     probs, probs_av = 0, 0
 
-    if not isinstance(s, str):
-        s = s.read()
-
-    if symbols_allowed(s):
+    if check_symbols(s):
         x = np.asarray(_get_features(s)).reshape(1, -1)
         for clf in clfs:
-            _initialize(clf)
-            clf = joblib.load(_name(clf))
+            _check_clf(clf)
+            clf = joblib.load(os.path.join(dirname, ('trained_clfs/' + clf.__class__.__name__)))
             probs = np.squeeze(clf.predict_proba(x))[1]
             probs_av += probs
-            if log:
-                logging.info(s + ' ' + _name(clf) + ': ' + '%.2f' % probs)
+            logging.info(s + ' ' + clf.__class__.__name__ + ': ' + '%.2f' % probs)
     else:
-        if log:
-            logging.info(s + ' has prohibited for b64 symbols ' + '%.2f' % probs)
+        logging.info(s + ' has prohibited for b64 symbols ' + '%.2f' % probs)
 
     if probs_av/len(clfs) > 0.5:
         return True
     return False
 
 
-def pick_out(s, index=True, clfs=classifiers):
+def pick_out(s, clfs=classifiers):
+    """Picks out words in s which are b64 encoded
+
+    Arguments:
+    s -- string
+
+    Returns:
+    b64 -- list with b64 words
+    notb64 -- list with other words
+    """
     b64, notb64 = [], []
-    ind = []
-
-    if not isinstance(s, str):
-        s = s.read()
-
-    s = s.split()
-    i = 0
-    for word in s:
-        i += 1
-        if isb64(word, log=False, clfs=clfs):
+    for word in s.split():
+        if isb64(word, clfs=clfs):
             b64.append(word)
-            ind.append(i)
         else:
             notb64.append(word)
-    if index:
-        return ind
-    else:
-        return b64, notb64
+    return b64, notb64
+
+
+def pick_out42(s, clfs=classifiers):
+    """Splits s into 42 symbols long fragments and returns indexes of b64 fragments
+
+    Arguments:
+    s -- string
+
+    Returns:
+    ind -- list with b64 fragments indexes
+    """
+    ind = []
+    index = 0
+    for fragment in [s[i:i + 42] for i in range(0, len(s), 42)]:
+        if isb64(fragment, clfs=clfs):
+            ind.append(index)
+        index += 1
+    return ind
 
 
 def delete_clfs(clfs=classifiers):
+    """Deletes all existing trained classifiers"""
     for clf in clfs:
-        if os.path.isfile(_name(clf)):
-            os.remove(_name(clf))
-
-
-def _create_xy(fill):
-    X, y = [], []
-    for row in fill:
-        y.append(int(row[0]))
-        X.append(_get_features(row[2:-1]))
-    return X, y
+        if os.path.exists(os.path.join(dirname, ('trained_clfs/' + clf.__class__.__name__))):
+            os.remove(os.path.join(dirname, ('trained_clfs/' + clf.__class__.__name__)))
 
 
 def _plot_roc(name_clf, y_test, y_pred):
+    """Plots ROC Curve and returns ROC Area"""
     fpr, tpr, _ = roc_curve(y_test, y_pred)
     roc_auc = auc(fpr, tpr)
 
@@ -131,29 +156,36 @@ def _plot_roc(name_clf, y_test, y_pred):
 
     return roc_auc
 
-
-def _clf_train(clfs, X_train, y_train):
-    for clf in clfs:
-        clf.fit(X_train, y_train)
-        joblib.dump(clf, _name(clf))  # Сохранение обученных классификаторов
-
-
-def _clf_test(clfs, X_test, y_test):
-    for clf in clfs:
-        y_pred = clf.predict(X_test)
-        acc = accuracy_score(y_test, y_pred)
-        roc_auc = _plot_roc(_name(clf), y_test, y_pred)
-        logging.info(_name(clf) + ' Accuracy: ' + '%.2f' % (100 * acc) + ' %. ROC Area: ' + '%5f' % roc_auc + '.')
-
-
-def main(test=True, clfs=classifiers):
-    fill = open('dataset.txt', 'r', encoding='utf-8')
-    X, y = _create_xy(fill)
-    fill.close()
+def _get_dataset():
+    """Reads dataset from file"""
+    y = list(map(int, config.get('dataset', 'y').split()))
+    X = list(map(_get_features, config.get('dataset', 'X').split()))
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=.3, random_state=0)
-    _clf_train(clfs, X_train, y_train)
-    if test:
-        _clf_test(clfs, X_test, y_test)
+    return X_train, X_test, y_train, y_test
+
+
+def _clf_train(clf, X_train=None, y_train=None):
+    """Trains and saves classifier"""
+    if X_train is None or y_train is None:
+        X_train, _, y_train, _ = _get_dataset()
+    clf.fit(X_train, y_train)
+    joblib.dump(clf, os.path.join(dirname, ('trained_clfs/' + clf.__class__.__name__)))  # Сохранение обученных классификаторов
+
+
+def _clf_test(clf, X_test, y_test):
+    """Test classifier"""
+    y_pred = clf.predict(X_test)
+    acc = accuracy_score(y_test, y_pred)
+    roc_auc = _plot_roc(clf.__class__.__name__, y_test, y_pred)
+    logging.info(clf.__class__.__name__ + ' Accuracy: ' + '%.2f' % (100 * acc)
+                 + ' %. ROC Area: ' + '%5f' % roc_auc + '.')
+
+
+def main(clfs=classifiers):
+    X_train, X_test, y_train, y_test = _get_dataset()
+    for clf in clfs:
+        _clf_train(clf, X_train, y_train)
+        _clf_test(clf, X_test, y_test)
 
 
 if __name__ == '__main__':
